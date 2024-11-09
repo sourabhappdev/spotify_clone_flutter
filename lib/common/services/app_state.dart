2cb2ce499d0dafb26e58d82763f1d00e2d733e5f
Sourabh Sharma <work.sourabhsharma@gmail.com>
2024-11-09 16:47:43 +0530

    [Add] getters,setter in appstate

--- lib/common/services/app_state.dart
+++ lib/common/services/app_state.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:spotify_clone/core/configs/constants/string_res.dart';
+
 import '../../core/configs/manager/storage_manager.dart';
 
 class AppState {
@@ -9,17 +11,42 @@ class AppState {
 
   final Completer<void> appInitialization = Completer<void>();
 
-  String sessionId = '';
-  String userId = '';
+  // Private variables
+  String _sessionId = '';
+  String _userId = '';
+  List<String> _likedSongs = [];
 
+  // Getter for sessionId
+  String get sessionId => _sessionId;
+
+  // Setter for sessionId (arrow function)
+  set setSessionId(String value) => _sessionId = value;
+
+  // Getter for userId
+  String get userId => _userId;
+
+  // Setter for userId (arrow function)
+  set setUserId(String value) => _userId = value;
+
+  // Getter for likedSongs
+  List<String> get likedSongs => _likedSongs;
+
+  // Setter for likedSongs (arrow function)
+  set setLikedSongs(List<String> value) => _likedSongs = value;
+
+  // Initialize values
   Future<void> setInitialValues() async {
-    sessionId = await StorageManager.instance.getData('sessionId') ?? '';
-    userId = await StorageManager.instance.getData('userId') ?? '';
+    _sessionId =
+        await StorageManager.instance.getData(StringRes.sessionId) ?? '';
+    _userId = await StorageManager.instance.getData(StringRes.userId) ?? '';
+    _likedSongs =
+        await StorageManager.instance.getList(StringRes.likedSongs) ?? [];
   }
 
+  // Clear all values
   Future<void> clearAllValues() async {
-    userId = '';
-    sessionId = '';
+    _userId = '';
+    _sessionId = '';
     await StorageManager.instance.clearData();
   }
 }
